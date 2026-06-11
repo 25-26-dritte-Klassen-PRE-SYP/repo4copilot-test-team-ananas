06a2a76afc6470f57b129471572c7dc9fe7a32af
Moritz Weißenhofer <moritz.weissenhofer@proton.me>
2026-06-11 11:18:14 +0200

    Set up Express server with basic route

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("Hallo Ananas!");
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Server läuft auf Vercel!');
+});
+
+// Wichtig für lokales Testen, Vercel handling ist serverless
+app.listen(PORT, () => {
+  console.log(`Server läuft auf Port ${PORT}`);
+});
+
+module.exports = app; // Wichtig für Vercel!
